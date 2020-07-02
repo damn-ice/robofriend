956af2470c493890f913f5503ea942ac058de289
IcE <joelik18@gmail.com>
2020-07-02 16:52:00 +0100

    Had to add an encryption to AJAX request

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,7 +22,7 @@ class App extends Component {
     }
 
     componentDidMount () {
-        fetch("http://jsonplaceholder.typicode.com/users")
+        fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
             .then(user => this.setState({robots: user})
         );
