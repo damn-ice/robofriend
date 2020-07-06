1cf570ef3246f2bbf48aaaee98d4f3231e7f1b5d
IcE <joelik18@gmail.com>
2020-07-06 18:24:21 +0100

    Implement Redux for State Management.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,52 @@
 import React, {Component} from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 // import { robots } from "./robots";
 import SearchBox from "../components/SearchBox";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css";
 import Scroll from "../components/Scroll";
+import { setSearchField, requestRobots} from "../actions";
+
+// Deals with the state which is gotten from the store...
+const mapStateToProps = state => {
+    return {
+        // The combiner makes us use state.searchRobots. ..
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+// Updates the action based on state gotten from store...
+const mapDispatchToProps = dispatch => {
+    return{
+      onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    // This will pass through thunk... because the action returns a function stead of an object...
+      onRequestRobots: () => dispatch(requestRobots())
+    }
+}
 
 
 class App extends Component {
-    constructor () {
-        super()
-        this.state = {
-            robots: [],
-            searchField: "",
-        };
-    }
-    // Some React specific syntax... => was used so the "this" is for where it was created...
-    onSearchChange = (event) => {
-        // Below is how state is changed in react and not this.state.searchField = ...
-        this.setState({searchField: event.target.value}); 
-    }
-
+   
     componentDidMount () {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then(response => response.json())
-            .then(user => this.setState({robots: user})
-        );
+        this.props.onRequestRobots();
     }
 
     render () {
-        const {robots, searchField} = this.state;
+        // const {robots} = this.state;
+        const {searchField, onSearchChange, robots, isPending} = this.props;
         const filteredRobots = robots.filter(robot => robot.name.toLowerCase()
             .includes(searchField.toLowerCase()));
-        if (robots.length === 0) {
+        if (isPending) {
             return (<h1 className="f1 tc"> Loading...</h1>);
         }else {
             return (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
-                    <SearchBox search={this.onSearchChange}/>
+                    <SearchBox search={onSearchChange}/>
                     <Scroll>
                         <ErrorBoundary>
                             <CardList robots={filteredRobots}/>
@@ -52,4 +60,29 @@ class App extends Component {
 
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
+//                uSInG  sTaTe  iN  rEAcT...,
+
+ // constructor () {
+    //     super()
+    //     this.state = {
+    //         robots: [],
+    //         // searchField: "",
+    //     };
+    // }
+    // onSearchChange now replaced with redux...
+    // // Some React specific syntax... => was used so the "this" is for where it was created...
+    // onSearchChange = (event) => {
+    //     // Below is how state is changed in react and not this.state.searchField = ...
+    //     this.setState({searchField: event.target.value}); 
+    // }
+
+
+    // componentDidMount () {
+        // fetch("https://jsonplaceholder.typicode.com/users")
+        //     .then(response => response.json())
+        //     .then(user => this.setState({robots: user})
+        // );
+    // }
